dbbd7649a0685a52fb1b47ee8ba2b4f42c36e936
Muhammad Sameer <136972597+Sameer-khan0@users.noreply.github.com>
2025-01-01 18:53:23 +0500

    variables in go

--- tempCodeRunnerFile.go
+++
@@ -1,1 +0,0 @@
-PrintIn
---
+++ variables in go/Variables.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Variables in Golang")
+	// the var is use for declearing the variable and then the variable name and then the type of the variable
+
+	var name string = "Sameer"
+	var age int = 21
+	height := 5.8
+	fmt.Println("My name is", name, "and I am", age, "years old and my height is", height, ".")
+}
