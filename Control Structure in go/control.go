af8989a3b549d11fc70b7312ecaafbc624595804
Muhammad Sameer <136972597+Sameer-khan0@users.noreply.github.com>
2025-01-02 10:35:14 +0500

    learning go

--- Control Structure in go/control.go
+++ Control Structure in go/control.go
@@ -3,6 +3,10 @@ package main
 import "fmt"
 
 func main() {
+	nums := []int{1, 2, 3, 4, 5}
+	for index, value := range nums {
+		fmt.Printf("index: %d, value: %d\n", index, value)
+	}
 	var age int = 1
 	grade := 'C'
 	if age >= 10 {
