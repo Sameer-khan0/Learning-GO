ee66905fbc5eb2516d7002600a1ec057c63a595c
Muhammad Sameer <136972597+Sameer-khan0@users.noreply.github.com>
2025-01-03 10:25:50 +0500

    update

--- Backend in go/main.go
+++ Backend in go/main.go
@@ -1,19 +1,30 @@
 package main
 
 import (
+	"backend-in-go/mypackage"
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Hello, Go! jdakjd anjksdajsdjkasn")
+	r.HandleFunc("/hello/{num1}/{num2}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		num1, err1 := strconv.Atoi(vars["num1"])
+		num2, err2 := strconv.Atoi(vars["num2"])
+		if err1 != nil || err2 != nil {
+			http.Error(w, "Invalid numbers provided in the URL", http.StatusBadRequest)
+			return
+		}
+		result := mypackage.Add(num1, num2)
+		fmt.Fprintf(w, "Result of %d and %d is %d", num1, num2, result)
 	})
 	r.HandleFunc("/hello/{name}", func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)
+		fmt.Fprintf(w, "Hello, %s! you are great!", vars["name"])
 		fmt.Fprintf(w, "Hello, %s! you are great!", vars["name"])
 	})
 	r.HandleFunc("/hello2", func(w http.ResponseWriter, r *http.Request) {
